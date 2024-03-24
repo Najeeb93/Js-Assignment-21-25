73af15fe134c5d2f00adf653b95faeaa00b636ac
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-24 20:48:06 +0500

    add to commit upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+//----ASSIGNMENT # 21-25 ------
+
+//---- Question No: 1
+
+// var firstName = prompt("Please enter your first name: ");
+// var  lastName = prompt("Please enter your last name: ");
+
+// var fullName = firstName + " " + lastName;
+
+// alert("hello " + fullName + "!")
+
+
