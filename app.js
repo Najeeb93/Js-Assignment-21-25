8ecffec03ad9d58e4a63c86642301f5152e23748
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-04 20:00:51 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -169,8 +169,16 @@
 
 //--- Question No: 1 --- //
 
-var num = 3.45214;
-document.write("number: " + num  + "<br>");
-document.write("round off value: " + Math.round(num) + "<br>");
+// var num = 3.45214;
+// document.write("number: " + num  + "<br>");
+// document.write("round off value: " + Math.round(num) + "<br>");
+// document.write("floor value: " + Math.floor(num) + "<br>");
+// document.write("ceil value: " + Math.ceil(num) + "<br>");  
+
+//---- Question No: 2 ---- //
+
+var num = -2.673;
+document.write("number: " + num + "<br>");
+document.write("round off value: " + Math.round(num)  + "<br>");
 document.write("floor value: " + Math.floor(num) + "<br>");
-document.write("ceil value: " + Math.ceil(num) + "<br>");  
+document.write("ceil value: " + Math.ceil(num) +"<br>");
