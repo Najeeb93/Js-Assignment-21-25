1922f42fb15e2565554dab63cd3b301c85af6e88
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-08 20:49:27 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -384,30 +384,30 @@
 
 // ---- Question No: 14 --- //
 
-var customerName = prompt("Enter your Name");
+// var customerName = prompt("Enter your Name");
 
-var monthNames = ["January","February","March","April", "May", "June","July","August","September","October","November","December"];
+// var monthNames = ["January","February","March","April", "May", "June","July","August","September","October","November","December"];
 
-var month = new Date().getMonth();
+// var month = new Date().getMonth();
 
-monthNames = monthNames[month];
+// monthNames = monthNames[month];
 
-var units = +prompt("Enter Number of Units");
+// var units = +prompt("Enter Number of Units");
 
-var chargesPerUnits = 16;
+// var chargesPerUnits = 16;
 
-var netAmount = units * chargesPerUnits;
+// var netAmount = units * chargesPerUnits;
 
-var latePaymentCharges = 350;
+// var latePaymentCharges = 350;
 
-var grossAmountPay = netAmount + latePaymentCharges;
+// var grossAmountPay = netAmount + latePaymentCharges;
 
-document.write(`Customer Name: ${customerName} <br>`);
-document.write(`Months: ${monthNames} <br>`);
-document.write(`Number of units: ${units} <br>`);
-document.write(`Charges per units: ${chargesPerUnits} <br>`);
-document.write(`Net Amount Payable (Within Due Date): ${netAmount} <br>`);
-document.write(`Late payment surcharges: ${latePaymentCharges} <br>`);
-document.write(`Gross Amount Payable (after Due Date): ${grossAmountPay}`);
+// document.write(`Customer Name: ${customerName} <br>`);
+// document.write(`Months: ${monthNames} <br>`);
+// document.write(`Number of units: ${units} <br>`);
+// document.write(`Charges per units: ${chargesPerUnits} <br>`);
+// document.write(`Net Amount Payable (Within Due Date): ${netAmount} <br>`);
+// document.write(`Late payment surcharges: ${latePaymentCharges} <br>`);
+// document.write(`Gross Amount Payable (after Due Date): ${grossAmountPay}`);
 
 
