2dd7919841df97fe6bc3b73dc7cf38f92f7885a3
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-26 18:22:27 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -439,6 +439,6 @@
 // };
 // console.log(test());
 // console.log(num);
-const numbers = [1,2,3,4];
-const [x] = numbers;
-console.log(x);
+// const numbers = [1,2,3,4];
+// const [x] = numbers;
+// console.log(x);
