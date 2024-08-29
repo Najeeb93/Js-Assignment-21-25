e77708c33d16a5141e82fcaf8a3da0a6d1b06edb
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-29 18:15:01 +0500

    add to commit

--- app.js
+++ app.js
@@ -471,9 +471,9 @@
 // let b = a;
 // a = 10;
 // console.log(b);
-var obj = {
-    a: 1,
-    b: 2,
-    a: 3
-};
-console.log(obj.a);
+// var obj = {
+//     a: 1,
+//     b: 2,
+//     a: 3
+// };
+// console.log(obj.a);
