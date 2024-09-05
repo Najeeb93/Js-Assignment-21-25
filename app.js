215a7163191aead42c92ad8ac72cb63c5254ebe7
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-05 17:06:18 +0500

    commit to change

--- app.js
+++ app.js
@@ -539,3 +539,11 @@
 // console.log(++b);
 // console.log([] == false);
 // console.log({} + {});
+
+var x = Math.floor(Math.random());
+if(x > 0.5) {
+    var x = 1;
+}else {
+    var x = 2;
+}
+console.log(x);
