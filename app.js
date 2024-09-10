3a63428380a56487312dfbdc785e172c8a7e514c
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-10 17:45:04 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -588,6 +588,6 @@
 // let b = a++;
 // let c = a + b;
 // console.log(c);
-const arr1 = [1,2,3];
-const arr2 = [...arr1, 4,5];
-console.log(arr2);
+// const arr1 = [1,2,3];
+// const arr2 = [...arr1, 4,5];
+// console.log(arr2);
