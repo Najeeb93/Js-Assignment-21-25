92c1a7ef109276d5864920ca9a354bc88a8751ed
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-28 19:00:28 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -667,7 +667,7 @@
 // let  x = 6;
 // x++;
 // console.log(x);
-let number = 0;
-console.log(number++);
-console.log(++number);
-console.log(number);
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
