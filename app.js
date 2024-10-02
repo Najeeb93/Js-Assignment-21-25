e26cd21c1091fbb0851b4afc0d1a2ddd4a4c40a5
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-02 18:37:39 +0500

    update app

--- app.js
+++ app.js
@@ -726,7 +726,7 @@
 //         console.log('i is 5 now');
 //     }
 // }
-const arr = [1,2,3];
-arr[10] = 10;
-console.log(arr.length);
-console.log(arr[9]);
+// const arr = [1,2,3];
+// arr[10] = 10;
+// console.log(arr.length);
+// console.log(arr[9]);
