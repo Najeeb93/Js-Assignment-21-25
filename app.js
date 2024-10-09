b201f6709f94d674e85c1471319d5f585f990052
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-09 18:36:39 +0500

    update app

--- app.js
+++ app.js
@@ -842,6 +842,6 @@
 // arr[10] = 10;
 // console.log(arr.length);
 // console.log(arr[9]);
-let a = typeof 1;
-let b = typeof a;
-console.log(b);
+// let a = typeof 1;
+// let b = typeof a;
+// console.log(b);
