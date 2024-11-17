bc56a823775cebaae6945467177a4286c73dc9e4
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-17 18:13:28 +0500

    add to commit

--- app.js
+++ app.js
@@ -997,6 +997,6 @@
 // let{a,b} = {a: 4, b: 8};
 // let c = b - a;
 // console.log(c);
-const arr1 = [1,2,3];
-const arr2 =[...arr1, 4,5];
-console.log(arr2);
+// const arr1 = [1,2,3];
+// const arr2 =[...arr1, 4,5];
+// console.log(arr2);
