437f5fa56747903f5bf3ecc1931271936ac66c82
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-04 18:11:32 +0500

    add to commit

--- app.js
+++ app.js
@@ -1048,7 +1048,7 @@
 //     console.log(4);
 // }
 // sample();
-let a = 2;
-let b = a;
-let c = (a + b) / 2;
-console.log((a + b + c) / 2);
+// let a = 2;
+// let b = a;
+// let c = (a + b) / 2;
+// console.log((a + b + c) / 2);
