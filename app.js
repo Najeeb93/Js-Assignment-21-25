88a39a8c5adfdfb1329b0e0599ac5a353c95455e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-10 17:41:35 +0500

    add to commit

--- app.js
+++ app.js
@@ -1057,9 +1057,9 @@
 // b = 10;
 // console.log(a);
 // console.log(b);
-const cars = ['BMW','Volvo','Mini'];
-let test = '';
-for(let x of cars) {
-    test += x; 
-}
-console.log(test);
+// const cars = ['BMW','Volvo','Mini'];
+// let test = '';
+// for(let x of cars) {
+//     test += x; 
+// }
+// console.log(test);
