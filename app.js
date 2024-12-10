6a1213449e7d11d21fc6de4e63b667c667ed7d53
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-10 17:57:31 +0500

    update

--- app.js
+++ app.js
@@ -1063,3 +1063,9 @@
 //     test += x; 
 // }
 // console.log(test);
+const person = {fname: 'John', lname: 'Doe', age: 25};
+let text = '';
+for(let x in person) {
+    text += person[x];
+}
+console.log(text);
