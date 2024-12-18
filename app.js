94828a5105143b0c5f8aeb20d524df4d70376366
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-18 20:07:54 +0500

    add to commit

--- app.js
+++ app.js
@@ -1112,11 +1112,11 @@
 // Leap year 366 days
 // Normal year has 365 days
 
-function isLeapYear(year) {
-    if(year % 4 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-console.log(isLeapYear(2024));
+// function isLeapYear(year) {
+//     if(year % 4 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isLeapYear(2024));
