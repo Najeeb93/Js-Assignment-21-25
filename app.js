315c47613fd2c8cb90ffaaa69398f31b0ec53de0
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-26 18:13:00 +0500

    add to commit

--- app.js
+++ app.js
@@ -1149,6 +1149,9 @@
 
 function printMultiplicationTable(tableOf, tableTill) {
     console.log(tableOf, tableTill);
+    for(let i = 1; i <= tableTill; i++) {
+        console.log(`${tableOf} * ${i} = ${i * tableOf}`);
+    }
 }
 
-printMultiplicationTable(4, 10)
+printMultiplicationTable(3, 10)
